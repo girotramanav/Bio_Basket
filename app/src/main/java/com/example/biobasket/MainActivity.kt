82cecf56e5a661e338b2bf997b92aa3dffe658e3
Manav Girotra <girotramanav22@gmail.com>
2021-02-21 22:30:55 +0530

    Final Commit

--- app/src/main/java/com/example/biobasket/MainActivity.kt
+++ app/src/main/java/com/example/biobasket/MainActivity.kt
@@ -1,11 +1,230 @@
 package com.example.biobasket
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavHost
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var onHome = true
+    var onCart = false
+    var onList = false
+    var onItem = false
+    var onFaq = false
+    var onContact = false
+    var onProfile = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppBarConfiguration(findNavController(R.id.host_fragment).graph)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.host_fragment) as NavHost
+        val navController = navHostFragment.navController
+        findViewById<NavigationView>(R.id.nav_view)
+            .setupWithNavController(navController)
+
+        val currentUser = intent.getStringExtra("currentUser")
+        Log.i("UserCurrent",currentUser.toString())
+
+        bottomNav.setOnNavigationItemSelectedListener { item->
+            when(item.itemId)
+            {
+                R.id.home-> {
+                    when{
+                        onCart->{
+                            host_fragment.findNavController().navigate(R.id.action_cartFragment_to_homeFragment)
+                        }
+                        onList->{
+                            host_fragment.findNavController().navigate(R.id.action_listFragment_to_homeFragment)
+                        }
+                        onItem->{
+                            host_fragment.findNavController().navigate(R.id.action_itemFragment_to_homeFragment)
+                        }
+                        onContact->{
+                            host_fragment.findNavController().navigate(R.id.action_contactFragment_to_homeFragment)
+                        }
+                        onProfile->{
+                            host_fragment.findNavController().navigate(R.id.action_profileFragment_to_homeFragment)
+                        }
+                        onFaq->{
+                            host_fragment.findNavController().navigate(R.id.action_faqFragment_to_homeFragment)
+                        }
+                    }
+                    true}
+                R.id.cart->{
+                    when{
+                        onHome->{
+                            host_fragment.findNavController().navigate(R.id.action_homeFragment_to_cartFragment)
+                        }
+                        onItem->{
+                            host_fragment.findNavController().navigate(R.id.action_itemFragment_to_cartFragment)
+                        }
+                        onList->{
+                            host_fragment.findNavController().navigate(R.id.action_listFragment_to_cartFragment)
+                        }
+                        onContact->{
+                            host_fragment.findNavController().navigate(R.id.action_contactFragment_to_cartFragment)
+                        }
+                        onProfile->{
+                            host_fragment.findNavController().navigate(R.id.action_profileFragment_to_cartFragment)
+                        }
+                        onFaq->{
+                            host_fragment.findNavController().navigate(R.id.action_faqFragment_to_cartFragment)
+                        }
+                    }
+                    true }
+                else->false
+            }
+        }
+
+        nav_view.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_home ->
+                            { when{
+                                onCart->{
+                                    host_fragment.findNavController().navigate(R.id.action_cartFragment_to_homeFragment)
+                                }
+                                onList->{
+                                    host_fragment.findNavController().navigate(R.id.action_listFragment_to_homeFragment)
+                                }
+                                onItem->{
+                                    host_fragment.findNavController().navigate(R.id.action_itemFragment_to_homeFragment)
+                                }
+                                onContact->{
+                                    host_fragment.findNavController().navigate(R.id.action_contactFragment_to_homeFragment)
+                                }
+                                onProfile->{
+                                    host_fragment.findNavController().navigate(R.id.action_profileFragment_to_homeFragment)
+                                }
+                                onFaq->{
+                                    host_fragment.findNavController().navigate(R.id.action_faqFragment_to_homeFragment)
+                                }
+                            }
+                                drawerLayout.close()
+                                true}
+                R.id.nav_cart ->
+                            {  when{
+                                onHome->{
+                                    host_fragment.findNavController().navigate(R.id.action_homeFragment_to_cartFragment)
+                                }
+                                onItem->{
+                                    host_fragment.findNavController().navigate(R.id.action_itemFragment_to_cartFragment)
+                                }
+                                onList->{
+                                    host_fragment.findNavController().navigate(R.id.action_listFragment_to_cartFragment)
+                                 }
+                                onContact->{
+                                    host_fragment.findNavController().navigate(R.id.action_contactFragment_to_cartFragment)
+                                }
+                                onProfile->{
+                                    host_fragment.findNavController().navigate(R.id.action_profileFragment_to_cartFragment)
+                                }
+                                onFaq->{
+                                    host_fragment.findNavController().navigate(R.id.action_faqFragment_to_cartFragment)
+                                }
+                            }
+                                drawerLayout.close()
+                                true}
+                R.id.nav_profile ->
+                            {when {
+                                onHome -> {
+                                    host_fragment.findNavController()
+                                        .navigate(R.id.action_homeFragment_to_profileFragment)
+                                }
+                                onItem -> {
+                                    host_fragment.findNavController()
+                                        .navigate(R.id.action_itemFragment_to_profileFragment)
+                                }
+                                onList -> {
+                                    host_fragment.findNavController()
+                                        .navigate(R.id.action_listFragment_to_profileFragment)
+                                }
+                                onCart -> {
+                                    host_fragment.findNavController()
+                                        .navigate(R.id.action_cartFragment_to_profileFragment)
+                                }
+                                onContact -> {
+                                    host_fragment.findNavController()
+                                        .navigate(R.id.action_contactFragment_to_profileFragment)
+                                }
+                                onFaq -> {
+                                    host_fragment.findNavController()
+                                        .navigate(R.id.action_faqFragment_to_profileFragment)
+                                }
+                            }
+                                drawerLayout.close()
+                                true}
+                R.id.nav_faq ->
+                            {
+                                when {
+                                    onHome -> {
+                                        host_fragment.findNavController()
+                                            .navigate(R.id.action_homeFragment_to_faqFragment)
+                                    }
+                                    onItem -> {
+                                        host_fragment.findNavController()
+                                            .navigate(R.id.action_itemFragment_to_faqFragment)
+                                    }
+                                    onList -> {
+                                        host_fragment.findNavController()
+                                            .navigate(R.id.action_listFragment_to_faqFragment)
+                                    }
+                                    onCart -> {
+                                        host_fragment.findNavController()
+                                            .navigate(R.id.action_cartFragment_to_faqFragment)
+                                    }
+                                    onContact -> {
+                                        host_fragment.findNavController()
+                                            .navigate(R.id.action_contactFragment_to_faqFragment)
+                                    }
+                                    onProfile -> {
+                                        host_fragment.findNavController()
+                                            .navigate(R.id.action_profileFragment_to_faqFragment)
+                                    }
+                                }
+                                drawerLayout.close()
+                                true}
+                R.id.nav_contact_us ->
+                            {when {
+                                onHome -> {
+                                    host_fragment.findNavController()
+                                        .navigate(R.id.action_homeFragment_to_contactFragment)
+                                }
+                                onItem -> {
+                                    host_fragment.findNavController()
+                                        .navigate(R.id.action_itemFragment_to_contactFragment)
+                                }
+                                onList -> {
+                                    host_fragment.findNavController()
+                                        .navigate(R.id.action_listFragment_to_contactFragment)
+                                }
+                                onCart -> {
+                                    host_fragment.findNavController()
+                                        .navigate(R.id.action_cartFragment_to_contactFragment)
+                                }
+                                onProfile -> {
+                                    host_fragment.findNavController()
+                                        .navigate(R.id.action_profileFragment_to_contactFragment)
+                                }
+                                onFaq -> {
+                                    host_fragment.findNavController()
+                                        .navigate(R.id.action_faqFragment_to_contactFragment)
+                                }
+                            }
+                                drawerLayout.close()
+                                true}
+                else -> {drawerLayout.close()
+                                true}
+            }
+        }
     }
 }
--- app/src/main/java/com/example/biobasket/SignInActivity.kt
+++ app/src/main/java/com/example/biobasket/SignInActivity.kt
@@ -1,11 +1,124 @@
 package com.example.biobasket
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
+    private var isLogin = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        signup.setOnClickListener{
+            if(isLogin)
+            {
+                heading.text = "SIGN UP"
+                signup.text = "LOGIN"
+                qText.text = "Already have an Account "
+                isLogin = false
+                name.visibility = View.VISIBLE
+                gapView.visibility = View.GONE
+            }
+            else
+            {
+                heading.text = "LOGIN"
+                signup.text = "SIGN UP"
+                qText.text = "Don't have an Account"
+                isLogin = true
+                name.visibility = View.GONE
+                gapView.visibility = View.VISIBLE
+            }
+        }
+
+        auth = Firebase.auth
+        signin.setOnClickListener {
+            if(isLogin)
+            {
+                signIn(email.text.toString(),password.text.toString())
+            }
+            else{
+                createAccount(name.text.toString(),email.text.toString(),password.text.toString())
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if(currentUser != null)
+        {
+            homePage()
+        }
+    }
+
+    private fun homePage() {
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun createAccount(name : String , email : String , password : String)
+    {
+        if(!validateForm())
+        {
+            Toast.makeText(this, "All fields are Required!!", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener {
+                if(it.isSuccessful)
+                {
+                    val user = hashMapOf("name" to name, "email" to email)
+                    val db = FirebaseFirestore.getInstance()
+                    db.collection("users").document(auth.currentUser!!.uid).set(user)
+                    homePage()
+                }
+                else
+                {
+                    Toast.makeText(this, it.exception?.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun signIn(email : String , password : String)
+    {
+        if(!validateForm())
+        {
+            Toast.makeText(this, "Valid Email id and password is required!!", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        auth.signInWithEmailAndPassword(email,password)
+            .addOnCompleteListener {
+                if(it.isSuccessful)
+                { homePage() }
+                else
+                { Toast.makeText(this, it.exception?.message, Toast.LENGTH_SHORT).show() }
+            }
+    }
+
+    private fun validateForm(): Boolean {
+        if(isLogin)
+        {
+            if (email.text.isEmpty() || password.text.isEmpty())
+                return false
+            return true
+        }
+        else {
+            if (email.text.isEmpty() || password.text.isEmpty() || name.text.isEmpty())
+                return false
+            return true
+        }
     }
 }
